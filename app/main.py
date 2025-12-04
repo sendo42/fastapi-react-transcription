ec62c6205ee582aec32fb03d65822cfaeb8f72e2
sendo42 <nyanyanya404@gmail.com>
2025-12-04 20:51:25 +0900

    pytestで確認

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 import uvicorn
 ##from uvicorn_config import config
 from fastapi.responses import HTMLResponse, RedirectResponse
-
+from pathlib import Path
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello" : "World"}
+    return {"Hello World" : "Hello World"}
 
 @app.get("/hello", response_class=HTMLResponse)
 def html():
     return "hello "
 
 
-@app.get("/input/audio")
-def inputAudio():
-    return "audio"
+UPLOAD_DIR = Path("./input")
+
+@app.post("/uploadAudio")
+async def uploadAudio(file: UploadFile = File(...)):
+    print("成功")
+    return {"filename" : file.filename}
